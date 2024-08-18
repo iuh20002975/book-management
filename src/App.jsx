2a17cd182110b4139eb938fd0dab3345d3749ec3
Nguyen Long <longsky0912624119@gmail.com>
2024-08-19 00:04:04 +0700

    Push the book management project, complete the user and book crud, there is no ordering part yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,132 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from "./redux/counter/counterSlice";
-import styles from './styles/Counter.module.css';
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import BookPage from './pages/book';
+import ContactPage from './pages/contact';
+import LoginPage from './pages/login';
+import { Outlet } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import RegisterPage from './pages/register';
+import { callFetchAccount } from './services/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { doGetAccountAction } from './redux/account/accountSlice';
+import Loading from './components/Loading';
+import NotFound from './components/NotFound';
+import AdminPage from './pages/admin';
+import ProtectedRoute from './components/ProtectedRoute';
+import LayoutAdmin from './components/Admin/LayoutAdmin';
+import './styles/reset.scss';
+import ManageUserPage from './pages/admin/user';
+import ManageBookPage from './pages/admin/book';
+
+const Layout = () => {
+  return (
+    <div className='layout-app'>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 export default function App() {
-  const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const isLoading = useSelector(state => state.account.isLoading)
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const getAccount = async () => {
+    if (
+      window.location.pathname === '/login'
+      || window.location.pathname === '/register'
+    )
+      return;
+
+    const res = await callFetchAccount();
+    if (res && res.data) {
+      dispatch(doGetAccountAction(res.data))
+    }
+  }
+
+  useEffect(() => {
+    getAccount();
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book/:slug",
+          element: <BookPage />,
+        },
+      ],
+    },
+
+    {
+      path: "/admin",
+      element: <LayoutAdmin />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true, element:
+            <ProtectedRoute>
+              <AdminPage />
+            </ProtectedRoute>
+        },
+        {
+          path: "user",
+          element:
+            <ProtectedRoute>
+              <ManageUserPage />
+            </ProtectedRoute>
+          ,
+        },
+        {
+          path: "book",
+          element:
+            <ProtectedRoute>
+              <ManageBookPage />
+            </ProtectedRoute>
+          ,
+        },
+      ],
+    },
+
+
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
-  );
+    <>
+      {
+        isLoading === false
+          || window.location.pathname === '/login'
+          || window.location.pathname === '/register'
+          || window.location.pathname === '/'
+          || window.location.pathname.startsWith('/book')
+          ?
+          <RouterProvider router={router} />
+          :
+          <Loading />
+      }
+    </>
+  )
 }
